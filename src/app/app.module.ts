a032b76e8ffd99d938e114234939a370f6048465
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-10-06 16:06:39 +0700

    Create and a bit header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input'; 
+import { MatInputModule } from '@angular/material/input';
+
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HeaderComponent } from './components/header/header.component'; 
 
 @NgModule({
   imports: [
@@ -24,6 +26,7 @@ import { MatInputModule } from '@angular/material/input';
   declarations: [
     AppComponent,
     LoginPageComponent,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
