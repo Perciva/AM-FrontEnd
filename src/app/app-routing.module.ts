4391fd58db5a11063843c035f1b2807835385fde
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-10-23 22:59:47 +0700

    Another fixing routing and add redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   { 
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full' 
+  },
+  { 
     path: 'login',
     component: LoginPageComponent 
   },
