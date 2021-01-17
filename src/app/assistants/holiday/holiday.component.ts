67fb54c6fb2d3377f7ea6131ccc583c7439ee5ab
ingeintania <ingeintania88@gmail.com>
2021-01-17 19:29:58 +0700

    Add Pop Up

--- src/app/assistants/holiday/holiday.component.ts
+++ src/app/assistants/holiday/holiday.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogHolidayComponent } from '../dialog/dialog-holiday/dialog-holiday.component';
 
 @Component({
   selector: 'app-holiday',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HolidayComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogHolidayComponent, {
+      width: '500px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      location.reload();
+      console.log('The dialog was closed');
+      console.log(result)
+    });
   }
 
 }
