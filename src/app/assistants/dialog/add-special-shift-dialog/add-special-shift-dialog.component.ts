596a17a4f081a1f889c721f345ce576b7d8dab51
ingeintania <ingeintania88@gmail.com>
2021-01-19 12:30:55 +0700

    add dialog

---
+++ src/app/assistants/dialog/add-special-shift-dialog/add-special-shift-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-special-shift-dialog',
+  templateUrl: './add-special-shift-dialog.component.html',
+  styleUrls: ['./add-special-shift-dialog.component.scss']
+})
+export class AddSpecialShiftDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
