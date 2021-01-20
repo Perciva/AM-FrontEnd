5c68c74cd96d2660d0ce6d581b9875d1be296d03
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2021-01-20 22:58:20 +0700

    Data Binding for Report (Minus Description & Work)

--- src/app/report/report-attendance/report-attendance.component.ts
+++ src/app/report/report-attendance/report-attendance.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { from } from 'rxjs';
 import { AssistantData } from 'src/app/common/assistant-model';
 import { GlobalConstants } from 'src/app/common/global-variable';
 import { AssistantService } from 'src/app/service/assistant-services.service';
 import { PeriodService } from 'src/app/service/period-services.service';
+import { ReportAttendanceService } from 'src/app/service/report-attendance-services.service';
+import { ReportData } from '../../common/report-model';
 
 var moment = require('moment');
 moment().format(); 
@@ -24,11 +27,14 @@ export class ReportAttendanceComponent {
   ast_initial;
   assistants: AssistantData[] = [];
   displayedColumns: string[] = ['date', 'day', 'description', 'working_hour', 'in', 'in_permission', 'out', 'out_permission', 'other', 'action'];
-  dataSource = new MatTableDataSource<AssistantData>(this.assistants);
+
+  report: ReportData[] = [];
+  dataSource = new MatTableDataSource<ReportData>(this.report);
 
   constructor(
     private periodService: PeriodService,
-    private assistantService: AssistantService
+    private assistantService: AssistantService,
+    private reportAttendanceService : ReportAttendanceService,
   ) { 
     var period_id = parseInt(localStorage.getItem(GlobalConstants.CURR_PERIOD));
     this.periodService.GetPeriodById(period_id).subscribe(async data => {
@@ -53,7 +59,6 @@ export class ReportAttendanceComponent {
       data.data.GetAssistantByPeriodId.forEach(element => {
         this.assistants.push(element)
       });
-      this.dataSource = new MatTableDataSource<AssistantData>(this.assistants);
     }
   }
 
@@ -70,12 +75,26 @@ export class ReportAttendanceComponent {
       }
     )
 
+    // var startDate = moment(this.startDate).add(7, 'hours')._d;
+    // var endDate = moment(this.endDate).add(7, 'hours')._d;
     //Get Report by the astId
-    
+    this.reportAttendanceService.GetAllAttendanceByDate(this.startDate, this.endDate, this.astId)
+    .subscribe(
+      async data=>{
+        await this.insertAttendanceData(data);
+      }
+    )
   }
   
   insertData(data){
     this.ast_initial = data.data.GetAssistantById.initial;
+  }
+  
+  insertAttendanceData(data){
+    data.data.GetAllAttendanceByDate.forEach(element => {
+      this.report.push(element);
+    });
+    this.dataSource = new MatTableDataSource<ReportData>(this.report);
     this.opened = true;
   }
 
@@ -85,6 +104,47 @@ export class ReportAttendanceComponent {
 
   isError(){
     return this.errors;
+  }
+
+  getInPermissionWord(s){
+    if(s == "IT")
+      return "Izin Telat Masuk"
+    if(s == "LM")
+      return "Lupa Absen Masuk"
+    if(s == "TM")
+      return "Telat Absen Masuk"
+    if(s == "TL")
+      return "Toleransi"
+    return "-";
+  }
+
+  getOutPermissionWord(s){
+    if(s == "IP")
+      return "Izin Pulang"
+    if(s == "LP")
+      return "Lupa Absen Pulang"
+    if(s == "TL")
+      return "Toleransi"
+    return "-";
+  }
+
+  getSpecialPermissionWord(s){
+    if(s == "CT")
+      return "Cuti"
+    if(s == "SK")
+      return "Sakit"
+    if(s == "AP")
+      return "Alpha"
+    if(s == "TL")
+      return "Toleransi"
+    return "-";
+  }
+
+  getDayOfTheWeek(s){
+    console.log(s)
+    console.log(moment(s).day());
+    return moment(s).day() == 0 ? 7 : moment(s).day();
+
   }
 
   doUpdate(element){
---
+++ src/app/service/report-attendance-services.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportAttendanceService {
+
+  constructor( private apollo: Apollo) { }
+
+  GetAllAttendanceByDate(start_date: string, end_date: string, assistant_id: number){
+    return this.apollo
+    .query<any>({
+      query: gql`
+      query GetAllAttendanceByDate(
+        $start_date: String!
+        $end_date: String!
+        $assistant_id: Int!
+      ) {
+        GetAllAttendanceByDate(
+          start_date: $start_date
+          end_date: $end_date
+          assistant_id: $assistant_id
+        ) {
+          attendance {
+            id
+            date
+            _in
+            _out
+            in_permission
+            out_permission
+            special_permission
+            in_permission_description
+            out_permission_description
+            special_permission_description
+            assistant {
+              initial
+              shift {
+                day
+                _in
+                _out
+              }
+              leader {
+                initial
+              }
+              period {
+                description
+              }
+            }
+          }
+          special_shift {
+            _in
+            _out
+            date
+          }
+        }
+      }
+      `,
+      variables: {
+        "start_date": start_date,
+        "end_date": end_date,
+        "assistant_id": assistant_id,
+      }
+    });
+  }
+}
