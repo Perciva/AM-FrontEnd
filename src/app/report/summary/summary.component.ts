3842453510855b7990bc0c0a669327d3a237a00d
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2021-01-20 19:33:16 +0700

    UI for Summary

--- src/app/report/summary/summary.component.ts
+++ src/app/report/summary/summary.component.ts
@@ -1,15 +1,100 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AssistantData } from 'src/app/common/assistant-model';
+import { GlobalConstants } from 'src/app/common/global-variable';
+import { LeaderData } from 'src/app/common/leader-model';
+import { AssistantPageComponent } from 'src/app/manage/assistant-page/assistant-page.component';
+import { AssistantService } from 'src/app/service/assistant-services.service';
+import { LeaderService } from 'src/app/service/leader-services.service';
+import { PeriodService } from 'src/app/service/period-services.service';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent implements OnInit {
+export class SummaryComponent {
 
-  constructor() { }
+  opened = false;
+  minDate;
+  maxDate;
+  startDate;
+  leaderId;
+  astId;
+  ast_initial;
+  leaders= [];
+  assistants= [];
+  displayedColumns: string[] = ['leader', 'assistant', 'ITM', 'LAM', 'TAM', 'TM', 'IP', 'LAP', 'TP', 'CT', 'SK', 'TL', 'AL', 'unverified'];
+  dataSource = new MatTableDataSource<AssistantData>(this.assistants);
 
-  ngOnInit(): void {
+  constructor(
+    private periodService: PeriodService,
+    private assistantService: AssistantService,
+    private leaderService: LeaderService,
+  ) { 
+    var period_id = parseInt(localStorage.getItem(GlobalConstants.CURR_PERIOD));
+    this.periodService.GetPeriodById(period_id).subscribe(async data => {
+      await this.insertDate(data);
+    })
+
+    this.assistantService.GetAllAssistant(period_id).subscribe(async data => {
+      await this.insertAssistantData(data);
+    });
+
+    this.leaderService.GetAllLeader(period_id).subscribe(async data => {
+      await this.insertLeaderData(data);
+    });
+  }
+
+  insertDate(data){
+    if(data.data.GetPeriodById != null){
+      this.startDate = this.minDate = data.data.GetPeriodById.start;
+      this.maxDate = data.data.GetPeriodById.end;
+    }
+  }
+  
+  insertAssistantData(data){
+    console.log(data.data);
+    if(data.data.GetAssistantByPeriodId != null){
+      // var temp = {id: 0, initial: "ALL"};
+      // this.assistants.push(temp)
+      data.data.GetAssistantByPeriodId.forEach(element => {
+        this.assistants.push(element)
+      });
+      this.dataSource = new MatTableDataSource<AssistantData>(this.assistants);
+    }
+  }
+  
+  insertLeaderData(data){
+    console.log(data.data);
+    if(data.data.GetLeaderByPeriodId != null){
+      // var temp = {id: 0, initial: "ALL"};
+      // this.leaders.push(temp);
+      data.data.GetLeaderByPeriodId.forEach(element => {
+        this.leaders.push(element)
+      });
+    }
+  }
+
+  viewData(){
+    console.log("Test Masuk");
+    // this.assistantService.GetAssistantById(this.astId).subscribe(
+    //   async data=>{
+    //     await this.insertData(data);
+    //   }
+    // )
+
+    //Get Report by the astId
+    this.opened = true;
+    
+  }
+  
+  insertData(data){
+    // this.ast_initial = data.data.GetAssistantById.initial;
+  }
+
+  isOpen(){
+    return this.opened;
   }
 
 }
